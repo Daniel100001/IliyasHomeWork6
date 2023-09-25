3f7d2ba5208af4f6974c4db5ca0a39e665710ba0
daniel <danchik@gmail.com>
2023-09-25 20:58:07 +0600

    Initial commit

---
+++ src/Weapon.java
@@ -0,0 +1,17 @@
+public class Weapon {
+    private String type;
+    private String name;
+
+    public Weapon(String type, String name) {
+        this.type = type;
+        this.name = name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
